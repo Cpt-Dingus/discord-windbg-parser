7653cdac2792563757ed79b0d9f0fec0f5041caf
Cpt-Dingus <meti17207@gmail.com>
2022-12-30 16:32:25 +0100

    v1.2 - Switched to discord.ext library, bot now uses slash commands with ! as a backup. Also fixed several major output and pasting related bugs, improved comments

--- DBGParser.py
+++ DBGParser.py
@@ -1,4 +1,10 @@
+# Made by Cpt-Dingus/Meti#7771
+# Helped on by members of the r/TechSupport Discord server
+print("v1.2 | 30-12-2022")
+
+
 import discord 
+from discord.ext import commands
 import os 
 import config as cfg 
 import zipfile 
@@ -8,19 +14,18 @@ import json
 import requests
 from datetime import datetime
 
-# Made by Cpt-Dingus/Meti#7771
-# Helped on by members of the r/TechSupport Discord server
-print("v1.1.2 | 20-08-2022")
+
+
+# --- Vars ---
 
 token = cfg.TOKEN
-channel = ""
+bot = commands.Bot(command_prefix='!',intents=discord.Intents.all()) # ALT PREFIX = !
 WINDBG_PATH = "C:\\Program Files (x86)\\Windows Kits\\10\\Debuggers\\x64\\windbg.exe"
-URL = "No results detected!"
 current_time = datetime.now().strftime("%H:%M:%S")
 total_dump_no = 0
 
 
-# Lowers the size of the windbg output to 1700 characters
+# Lowers the size of the windbg output to 1700 characters by excluding junk
 EXCLUDES = ['!Analyze -v', '!analyze', '*    ', '******', '......', '.......................', '.trap', 'Arg2', 'Arg3', 'Arg4', 'BLACKBOXBSD', 'BLACKBOXNTFS', 'BLACKBOXPNP',
             'BLACKBOXWINLOGON', 'BUCKET_ID', 'BUGCHECK', 'BUILDLAB_STR', 'Bugcheck Analysis', 'Copyright', 'Debug session', 'ERROR_CODE', 'EXCEPTION_CODE_STR',
             'EXCEPTION_PARAMETER1', 'EXCEPTION_STR', 'Edition', 'ExceptionAddress', 'ExceptionCode', 'ExceptionFlags', 'Executable search', 'FAILURE_ID_HASH', 'IMAGE_VERSION',
@@ -32,26 +37,35 @@ EXCLUDES = ['!Analyze -v', '!analyze', '*    ', '******', '......', '...........
 
 
 
+
+# --- Checks ---
+
+if not os.path.exists(WINDBG_PATH): raise FileNotFoundError('''10 -> Windbg not found, make sure the x64 version of the STANDARD (Not preview!) WinDBG editor is installed at
+"C:\\Program Files (x86)\\Windows Kits\\10\\Debuggers\\x64\\windbg.exe"''')
+
+
+# --- Defs ---
+
 def paste_file(file_path):
-	global URL
- 
+
 	headers = {
 		"Accept": "application/json",
-		"Linx-Randomize": "yes",
-	}
+		"Linx-Randomize": "yes"}
 
-	with open(file_path, 'rb') as f:
-		data = f.read()
+	with open(file_path, 'r') as f:
+		data = f"{f.read()}".replace(r'\n', '\n')
+
 
 	response = requests.put(f"https://paste.rtech.support/upload/{file_path}", headers=headers, data=data)
-	json_output = response.content.decode("utf8")
-	json_parsed = json.loads(json_output)
-	URL = json_parsed["url"]
+	json_parsed = json.loads(response.content.decode("utf8"))
+	return json_parsed["url"]
 
 
 
 def process_dump_file(dump_file_path, timeout_seconds=60):
-    # Bool to check if the file succeeded, string to check if file had a windbg error
+    # RETURNS:
+	#  -> Bool to check if the file succeeded
+	#  -> String to check if file had a windbg error
     
 	if not os.path.exists(dump_file_path) or not dump_file_path.endswith(".dmp"):
 		return False, ''
@@ -74,24 +88,31 @@ def process_dump_file(dump_file_path, timeout_seconds=60):
 		with open(debug_output_file, 'r') as windbg_output:
       
 			for line in windbg_output:
-				line = line.strip()
+				
+				if line == '\n': continue  # Removes newlines
     
-				if len(line) > 100 and ' : ' in line:  # Reduce stack trace
+    
+				elif len(line) > 100 and ' : ' in line:  # Reduce stack trace
 					tail = line.split(' : ', 1)[1]
-					filtered_result += f"{tail}\n"
+					filtered_result += f"{tail}"
      
 				elif "SYMBOL_NAME" in line:        # Separate stack trace from other important data
 					head, sep, tail = line.partition("SYMBOL_NAME:")
-					filtered_result += f"{head}\n\n----------\n{sep}{tail}"
+					filtered_result += f"{head}\n----------\n{sep}{tail}"
      
-				elif len(line) and not any(exclusion in line for exclusion in EXCLUDES):
-					filtered_result += f"{line}\n"
+				elif not any(exclusion in line for exclusion in EXCLUDES):
+					filtered_result += f"{line}"
      
+    
 		return filtered_result, 'OK'
 
 
 
 def process_dumps_from_zip(dump_zip_path, max_dump_size_bytes=(1024*1024*100), timeout_seconds=45):
+    # RETURNS:
+    #  -> List of all debugging results
+    #  -> Number of all dump files
+    
 	global total_dump_no 
 	total_dump_no = 0
  
@@ -128,89 +149,98 @@ def process_dumps_from_zip(dump_zip_path, max_dump_size_bytes=(1024*1024*100), t
 
 
 
-class MyClient(discord.Client):
 
-	async def on_ready(self):
-		print("Logged in as {0}".format(self.user))
+# --- Main ---
 
-	async def on_message(self, message):
-		if len(message.attachments) == 0: return
+@bot.hybrid_command(name="debug-files", description="Debugs a .dmp or a .zip containing .d	mp files, posts link with windbg output")
+async def debug_files(ctx, file: discord.Attachment):	
+	if len(ctx.message.attachments) == 0:	
+		await ctx.send("No attachments found")	
+	
+	for attach in ctx.message.attachments:	
+		filename = attach.filename	
+	
+		if filename.endswith(".dmp"):	
+			dump_no = 1	
+	
+			await ctx.send(".dmp file detected! Processing...")	
+			print("Parsing dump file..")	
+	
+			with tempfile.TemporaryDirectory() as tmpdir:	
+				dmp_file_name = os.path.join(tmpdir, f"{os.urandom(24).hex()}.dmp")	
+				await attach.save(fp=dmp_file_name)	
+				dump_result, res = process_dump_file(dmp_file_name)
+				print(dump_result)
+				if res == 'OK':
+					result_file_path = os.path.join(tmpdir, f"{os.urandom(24).hex()}txt")
+	
+					with open(result_file_path, "w") as results_file:
+							results_file.write(f"Debugged at: {current_time}\n")
+							results_file.write(f"{dump_result}")
+	
+	
+					await ctx.send(f"Result: {paste_file(result_file_path)}")
+	
+				else:
+					print('Dump failed to debug')
+					await ctx.send("Dump debugging failed!")
+	
 
-		for attach in message.attachments:
-			filename = attach.filename
-			client.get_channel(channel)
-   
-			if filename.endswith(".dmp"):
-				dump_no = 1
-    
-				await message.channel.send(".dmp file detected! Processing...")
-				print("Parsing dump file..")
-    
-				with tempfile.TemporaryDirectory() as tmpdir:
-					dmp_file_name = os.path.join(tmpdir, f"{os.urandom(24).hex()}.dmp")
-					await attach.save(fp=dmp_file_name)
-					dump_result = process_dump_file(dmp_file_name)
-    
-					if dump_result:
-						result_file_path = os.path.join(tmpdir, f"{os.urandom(24).hex()}txt")
-      
-						with open(result_file_path, "w") as results_file:
-								results_file.write(f"{dump_result}")
-        
-						paste_file(result_file_path)
-      
-						await message.channel.send(f"Result: {URL}")
-      
+		elif filename.endswith(".zip"):
+		
+			await ctx.send(f"Zip file detected! Processing, this might take a few minutes...")
+			print("Parsing zip file..")
+
+			with tempfile.TemporaryDirectory() as tmpdir:
+				dmp_zip_path = os.path.join(tmpdir, f"{os.urandom(24).hex()}.zip")
+	
+				await attach.save(fp=dmp_zip_path)
+				dump_results, total_dump_no = process_dumps_from_zip(dmp_zip_path)
+
+				dump_no = 0  # Number of current dump for console prints
+				dumps_ok = 0  # Number of succesful debug outputs for returned message
+
+				result_file_path = os.path.join(tmpdir, f"{os.urandom(24).hex()}.txt")
+				
+				with open(result_file_path, 'w') as results_file:
+					results_file.write(f"Debugged at: {current_time}\n")
+
+				for result in dump_results:
+					dump_no += 1
+
+					result = "".join(result)	# Converts result to a string
+
+					with open(result_file_path, 'a') as results_file:  # Adds results to file
+						results_file.write(f"\n{'-' * 20} Dump number {dump_no} {'-' * 20}\n")
+						results_file.write(result)
+
+					if result == 'FAIL':  
+						print(f"Dump #{dump_no} FAIL")
+
 					else:
-						print('Dump failed to debug')
-						await message.channel.send("Dump failed to debug!")
-      
-      
-			elif filename.endswith(".zip"):
-       
-				await message.channel.send(f"Zip file detected! Processing, this might a few minutes...")
-				print("Parsing zip file..")
-	
-				with tempfile.TemporaryDirectory() as tmpdir:
-					dmp_zip_path = os.path.join(tmpdir, f"{os.urandom(24).hex()}.zip")
-		
-					await attach.save(fp=dmp_zip_path)
-					dump_results, total_dump_no = process_dumps_from_zip(dmp_zip_path)
-
-					dump_no = 0  # Number of current dump for console prints
-					dumps_ok = 0  # Number of succesful debug outputs for returned message
-     
-					result_file_path = os.path.join(tmpdir, f"{os.urandom(24).hex()}.txt")
-					
-					with open(result_file_path, 'w') as results_file:
-						results_file.write(f"Debugged at: {current_time}\n")
-	
-					for result in dump_results:
-						dump_no += 1
-      
-						result = "".join(result)
-      
-						with open(result_file_path, 'a') as results_file:  # Adds data to file
-							results_file.write(f"\n{'-' * 20} Dump number {dump_no} {'-' * 20}\n")
-							results_file.write(result)
-       
-						if result == 'FAIL':  
-							print(f"Dump #{dump_no} FAIL")
-
-						else:
-							print(f"Dump #{dump_no} OK")
-							dumps_ok += 1
-		
+						print(f"Dump #{dump_no} OK")
+						dumps_ok += 1
 
 
-					if len(dump_results) > 0:
-						paste_file(result_file_path)
-      
-				await message.channel.send(f"{dumps_ok}/{total_dump_no} dump{'s' * dump_no > 1} succesfully debuggedd\nResults: {URL}")
-	
+				dump_str = "dump"
+				if dump_no > 1: dump_str = "dumps"
+
+				if len(dump_results) > 0:
+				
+					await ctx.send(f"{dumps_ok}/{total_dump_no} {dump_str} succesfully debugged\nResults: {paste_file(result_file_path)}")
+
+				else:
+					await ctx.send(f"None out of {total_dump_no} dumps succesfully debugged")
+
 				total_dump_no = 0
 					
-					
-if __name__ == '__main__':
-	client = MyClient()
-	client.run(token)
+				
+
+@bot.event
+async def on_ready():
+	await bot.tree.sync()
+	print(f"Logged in as {bot.user}")
+
+
+
+if __name__ == '__main__':	bot.run(token)
